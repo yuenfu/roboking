0f895026073881dfaf9a7a7396f3f1ba8dc51cd8
tonix0114 <tonix0114@gmail.com>
2016-09-12 10:54:22 +0900

    메인 헤더 get / view 함수

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include "roboking.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void get_main_structure(FILE *, struct stMainHeader *);
+void view_main_structure(struct stMainHeader);
 
 int main(int argc, char **argv)
 {
@@ -16,4 +22,30 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+	struct stMainHeader main_header;
+	get_main_structure(fp, &main_header);
+	view_main_structure(main_header);
+
 }
+
+void get_main_structure(FILE * fp, struct stMainHeader * main_header)
+{
+	if( fp )
+	{
+		fread(main_header, 1, sizeof(struct stMainHeader), fp); 	
+	}
+}
+
+void view_main_structure(struct stMainHeader header)
+{
+	printf("Revision : 0x%x\n", header.unRevision);
+	printf("FirmwareSize : 0x%x\n", header.unFirmwareSize);
+	printf("Checksum : 0x%x\n", header.unChecksum);
+	printf("szPartNumber : ");
+	for (int i = 0; ((unsigned char)header.szPartNumber[i]) != '\x00'; i++)
+	{
+		printf("%c", header.szPartNumber[i]);
+	}
+	printf("\n");
+}
+
